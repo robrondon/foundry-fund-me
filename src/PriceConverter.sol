8d0239a7a711e6c3f45dc933180eef4a7c5bb16e
Robert Rondon <robrondon11@gmail.com>
2025-06-10 01:05:38 -0300

    Adding library PriceConverter.sol with getPrice function

---
+++ src/PriceConverter.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    function getPrice() internal view returns (uint256) {
+        // address ETH/USD Sepolia 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+
+        (, int256 answer, , , , ) = priceFeed.latestRoundData();
+    }
+}
