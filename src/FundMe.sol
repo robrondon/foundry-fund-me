4d8752fed3594f432abf8a603163254b0f73855e
Robert Rondon <robrondon11@gmail.com>
2025-06-10 01:37:16 -0300

    Update FundMe.sol - Adding withdraw function and modifiers

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.24;
 
 import {PriceConverter} from "./PriceConverter";
 
+error FundMe__NotOwner();
+
 contract FundMe {
     using PriceConverter for uint256;
 
@@ -27,11 +29,32 @@ contract FundMe {
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
+    function withdraw() public onlyOwner {
+        for (uint256 i = 0; i < funders.length; i++) {
+            address funder = funders[i];
+            addressToAmountFunded[funder] = 0;
+        }
+        // Reset funders array
+        funders = new address[](0);
+
+        (bool success, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(success, "Withdraw failed");
+    }
+
     receive() external payable {
         fund();
     }
 
     fallback() external payable {
         fund();
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) {
+            revert FundMe__NotOwner();
+        }
+        _;
     }
 }
